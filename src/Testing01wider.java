b3e3cf8273d41879fa2d0406851d0976bdfd4d78
jack138138 <jackpeter138138@gmail.com>
2023-05-11 16:48:16 +0100

    Testing Wider

---
+++ src/Testing01wider.java
@@ -0,0 +1,5 @@
+public class Testing01wider {
+    public static void main(String[] args){
+        System.out.println("Thank you Wider Group");
+    }
+}
---
+++ src/Testingwider/Testingwider.java
@@ -0,0 +1,9 @@
+package Testingwider;
+
+public class Testingwider {
+    public static void main(String[] args){
+        System.out.println("Thank you Wider Group");
+    }
+
+
+}
